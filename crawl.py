f810671a273866a70db5e3bd81526deb55a728b4
SUSean0118 <susean0118@gmail.com>
2016-07-02 02:00:09 +0800

    pc

--- crawl.py
+++ crawl.py
@@ -3,8 +3,14 @@ from bs4 import BeautifulSoup
 import lxml
 import sys
 
+#印出
+def printHtml(tag) :
+	for item in tag :
+		print(item.text.encode(sys.stdin.encoding, "replace").decode(sys.stdin.encoding))
+
 #送出GET請求到遠端伺服器，伺服器接受請求後回傳<Response [200]>，代表請求成功
-res = requests.get("http://maplestage.com/")
+payload = {'w': 'test'}
+res = requests.post("http://yun.dreye.com/dict_new/dict.php", data=payload)
 
 #經過BeautifulSoup內lxml編輯器解析的結果
 soup = BeautifulSoup(res.text,'lxml')
@@ -13,7 +19,15 @@ soup = BeautifulSoup(res.text,'lxml')
 #print(soup.encode(sys.stdin.encoding, "replace").decode(sys.stdin.encoding)) 
 
 #使用select選取特定元素
-text = soup.select('q_m_title01')
-
-#印出
-print(text.encode(sys.stdin.encoding, "replace").decode(sys.stdin.encoding))
+tag = soup.select('#display_word span')
+printHtml(tag)
+tag = soup.select('#digest')
+printHtml(tag)
+tag = soup.select('#usual .sg')
+printHtml(tag)
+tag = soup.select('#usual .phrase')
+printHtml(tag)
+tag = soup.select('#usual .der')
+printHtml(tag)
+tag = soup.select('#usual .dif')
+printHtml(tag)
